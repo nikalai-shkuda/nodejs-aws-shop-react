c7dca3c7bad67cc9265af115c94d1a436502cf27
Nikalai Shkuda <nikolayshkuda@mail.ru>
2025-04-18 11:11:03 +0300

    feat(API): change API paths to BFF

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order:
-    "http://nikalai-shkuda-cart-api-development.eu-west-1.elasticbeanstalk.com/api/profile/cart",
-  import: "https://bjua3mwuuk.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://2ywop7id3j.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://nikalai-shkuda-cart-api-development.eu-west-1.elasticbeanstalk.com/api",
+  order: "http://nikalai-shkuda-bff-api-develop.eu-west-1.elasticbeanstalk.com",
+  import:
+    "http://nikalai-shkuda-bff-api-develop.eu-west-1.elasticbeanstalk.com",
+  bff: "http://nikalai-shkuda-bff-api-develop.eu-west-1.elasticbeanstalk.com",
+  cart: "http://nikalai-shkuda-bff-api-develop.eu-west-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
